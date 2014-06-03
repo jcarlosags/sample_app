8f8e252ec2cdf1631276237fd97eac58d34f7ee8
Juan Carlos <jcarlos.editor@gmail.com>
2014-06-03 10:42:55 -0500

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -19,13 +19,6 @@ describe "User pages" do
       it { should have_title(full_title('Sign up')) }
     end
 
-    describe "signup page" do
-        before { visit signup_path }
-
-        it { should have_content('Sign up') }
-        it { should have_title(full_title('Sign up')) }
-      end
-
     describe "signup" do
 
         before { visit signup_path }
@@ -36,6 +29,20 @@ describe "User pages" do
           it "should not create a user" do
             expect { click_button submit }.not_to change(User, :count)
           end
+        end
+        
+        describe "after submission" do
+          before { click_button submit}
+          
+          it { should have_title('Sign up') }
+          it { should have_content('error') }
+          it { has_content?('Password digest can\'t be blank') }
+          it { should have_content('Name can\'t be blank') }
+          it { should have_content('Email can\'t be blank') }
+          it { should have_content('Email is invalid') }
+          it { should have_content('Password can\'t be blank') }
+          it { has_content?('Password is to short (minimum is 6 characters)') }
+          it { has_content?('Password confirmation can\'t be blank') }
         end
 
         describe "with valid information" do
@@ -48,6 +55,15 @@ describe "User pages" do
 
           it "should create a user" do
             expect { click_button submit }.to change(User, :count).by(1)
+          end
+          
+          describe "after saving the user" do
+            before { click_button submit }
+            let(:user) { User.find_by(email: 'user@example.com') }
+            
+            it { should have_link('Sign out') }
+            it { should have_title(user.name) }
+            it { should have_selector('div.alert.alert-success', text: 'Welcome') }
           end
         end
       end
